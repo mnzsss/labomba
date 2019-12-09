2fbd32814ce5ea31ba95b40ba649f2c594395940
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2019-12-09 18:23:28 -0300

    style: 💄 Add Post Thumbnail in Blog Post Page

--- single.php
+++ single.php
@@ -2,34 +2,22 @@
 
 <?php if (have_posts()): while (have_posts()): the_post();?>
 
-<!-- Introdução -->
-<div class="container" id="blog-post">
-    <div class="row">
-        <div class="col-md-7 col-12">
-            <h2><?php the_title();?></h2>
-        </div>
-        <div class="col-4 d-none d-md-block">
-            <img src="<?php bloginfo('template_directory');?>/assets/img/labomba_logo_detail.svg" alt="Estúdio La.Bomba"
-                class="img-fluid logo-detail" />
-        </div>
-        <div class="col-md-8 col-sm-12" id="info">
-            <p class="pt-3">
-                <?php the_excerpt();?>
-            </p>
-            <a href="/blog">&larr; Voltar</a>
-        </div>
-        <div class="col-md-3 seta text-center flutuar">
-            <a href="#text">
-                <img src="<?php bloginfo('template_directory');?>/assets/img/seta.svg" alt="Viage!" class="img-fluid" />
-            </a>
-        </div>
-    </div>
-</div>
 </header>
 
 <section id="blog-text">
     <div class="container" id="text">
         <div class="row">
+            <div class="col-12">
+                <div class="destaque-post">
+                    <div class="destaque-post-image" style="background-image: url(<?php the_post_thumbnail_url()?>)">
+                    </div>
+                    <div class="destaque-post-info">
+                        <a href="<?php site_url()?>/blog">← Voltar</a>
+                        <h1><?php the_title();?></h1>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-md-9 col-12 conteudo">
                 <?php the_content();?>
 
