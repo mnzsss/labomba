d09c74976aaec21306288da32ec1887d179b612b
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2019-12-09 18:21:29 -0300

    fix: 🐛 Fix Headings from Case Pages
    Change colors of case pages

--- cases/inc/header-mineiro.php
+++ cases/inc/header-mineiro.php
@@ -1,18 +1,18 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 
 <head>
     <!-- Required meta tags -->
-    <meta <?php bloginfo('charset'); ?>>
+    <meta <?php bloginfo('charset');?>>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <meta name="theme-color" content="#ffffff" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/normalize.min.css" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/labomba.min.css" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/labomba-mineiro.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/normalize.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/labomba.min.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/labomba-mineiro.min.css" />
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
@@ -30,47 +30,11 @@
     gtag("config", "UA-129826334-1");
     </script>
 
-    <!-- Facebook Pixel Code -->
-    <script>
-    !(function(f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function() {
-            n.callMethod ?
-                n.callMethod.apply(n, arguments) :
-                n.queue.push(arguments);
-        };
+    <!-- AdSense -->
+    <script data-ad-client="ca-pub-8577584183802367" async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = "2.0";
-
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-
-        s.parentNode.insertBefore(t, s);
-    })(
-        window,
-        document,
-        "script",
-
-        "https://connect.facebook.net/en_US/fbevents.js"
-    );
-
-    fbq("init", "438763636979719");
-
-    fbq("track", "PageView");
-    </script>
-
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=438763636979719&ev=PageView&noscript=1" /></noscript>
-    <!-- End Facebook Pixel Code -->
-
-    <?php wp_head('coca'); ?>
+    <?php wp_head();?>
 </head>
 
 <body>
@@ -80,7 +44,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <a href="<?php echo site_url(); ?>">
-                        <img src="<?php bloginfo('template_directory'); ?>/assets/img/labomba_logo.svg"
+                        <img src="<?php bloginfo('template_directory');?>/assets/img/labomba_logo.svg"
                             alt="Estúdio LaBomba" class="img-fluid" />
                     </a>
                 </div>
--- cases/single-kalango.php
+++ cases/single-kalango.php
@@ -63,7 +63,7 @@
         <div class="row">
             <div class="col-12">
                 <img src="<?php bloginfo('template_directory');?>/assets/img/cases/kalango/elkalango_logo.svg"
-                    alt="El Kalango" class="img-fluid" data-aos="flip-up" />
+                    alt="El Kalango" class="img-fluid" />
             </div>
         </div>
     </div>
