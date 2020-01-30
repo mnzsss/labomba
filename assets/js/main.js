bb44d43821053636bf4a7e6a54adbe43c1c6243b
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-01-30 13:36:59 -0300

    chore: Add Babel with compile Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,40 @@
-// Mega Menu Icon
-$(".nav_icon").click(function() {
-  $(this).toggleClass("open");
-  $(".nav_menu").toggleClass("open");
+$(document).ready(function() {
+  // Mega Menu Icon
+  $(".nav_icon").click(function() {
+    $(this).toggleClass("open");
+    $(".nav_menu").toggleClass("open");
+    $(".header_footer").toggleClass("fixed");
+    $(".nav_content-logo").toggleClass("fixed");
+    $("body").toggleClass("hidden");
+  });
+
+  // Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+
+  // Scenes
+  var aboutScene = new ScrollMagic.Scene({
+    triggerElement: ".studio",
+    duration: "200%"
+  })
+    .setPin(".about_content")
+    .setClassToggle(".about_content", "fade-in")
+    .addTo(controller);
+
+  var magentaScene = new ScrollMagic.Scene({
+    triggerElement: "body",
+    duration: "300%",
+    triggerHook: 0.1
+  })
+    .setPin(".background_detail_magenta")
+    .addTo(controller);
+
+  var lampScene = new ScrollMagic.Scene({
+    triggerElement: ".studio",
+    triggerHook: 1
+  })
+    .setPin(".studio_lamp")
+    .setClassToggle(".studio_lamp", "fade-in")
+    .addTo(controller);
 });
 
 // Studio
@@ -11,14 +44,12 @@ $(window).on("scroll", () => {
   // Show video
   if ($(this).scrollTop() > windowHeight / 1.7) {
     $(".studio_video").css({ opacity: 0.4 });
-    $(".studio_lamp").css({ opacity: 1 });
   } else {
     $(".studio_video").css({ opacity: 0 });
-    $(".studio_lamp").css({ opacity: 0 });
   }
 
   // Change text about with scroll
-  if ($(this).scrollTop() > windowHeight * 1.12) {
+  if ($(this).scrollTop() > windowHeight * 1.3) {
     $(".about_content-paragraph").html(
       "Somos percepção e caminho, criamos conceitos, estratégias, acreditamos que a marca se comunica com o seu propósito, esse é o verdadeiro sentido."
     );
@@ -26,22 +57,5 @@ $(window).on("scroll", () => {
     $(".about_content-paragraph").html(
       "Nós aumentamos o potencial competitivo e resultados de nossos clientes, gerando ligação emocional entre consumidores e a marca através da ousadia, estratégia, arte e design"
     );
-  }
-
-  // Change elements universe section
-  if ($(this).scrollTop() > windowHeight * 1.7) {
-    $(".background_detail_magenta").css({
-      position: "absolute",
-      top: "10px",
-      left: "10px"
-    });
-    $(".background_detail_amarelo").css({ bottom: "70%" });
-  } else {
-    $(".background_detail_magenta").css({
-      position: "fixed",
-      top: "-14%",
-      left: "90%"
-    });
-    $(".background_detail_amarelo").css({ bottom: "8em" });
   }
 });
